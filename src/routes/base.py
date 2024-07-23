6fcbb15eb3ea2d724fc431ab0083dfe1e3e3e008
Abdelrahman-Alkhodary <abdelrahman.helmy2010@gmail.com>
2024-07-23 18:00:11 +0300

    still the file path wsl needs to be solved

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter, FastAPI, Depends
+from helpers.config import get_settings, Settings
 import os 
+
 
 
 base_router = APIRouter(
@@ -8,9 +10,10 @@ base_router = APIRouter(
 )
 
 @base_router.get("/")
-async def welcome():
-    app_name = os.getenv('APP_NAME')
-    app_version = os.getenv('APP_VERSION')
+async def welcome(app_settings: Settings = Depends(get_settings)):
+    
+    app_name = app_settings.APP_NAME
+    app_version = app_settings.APP_VERSION
     return {
         "app_name": app_name,
         "app_version": app_version,
